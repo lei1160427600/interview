ac97f3ea09e05b213603705ac6b5d64980ca02d1
lei1160427600 <xuechao_lei@126.com>
2019-07-19 23:15:01 +0800

    some change

---
+++ src/main/java/com/example/interview/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.interview.entity;
+
+import lombok.Data;
+
+/**
+ * @author xiaolei
+ * @Title: User
+ * @ProjectName interview
+ * @Description: TODO
+ * @date 2019/7/140:07
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+}
---
+++ src/test/java/com/example/interview/sort/MergeSort.java
@@ -0,0 +1,15 @@
+package com.example.interview.sort;
+
+/**
+ * @author xiaolei
+ * @Title: MergeSort
+ * @ProjectName interview
+ * @Description: 归并排序 时间复杂度 O(N*logN)   稳定性：稳定
+ * 基本思想：归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法的一个非常典型的应用。
+ * 首先考虑下如何将2个有序数列合并。这个非常简单，只要从比较2个数列的第一个数，谁小就先取谁，
+ * 取了后就在对应数列中删除这个数。然后再进行比较，如果有数列为空，那直接将另一个数列的数据依次取出即可。
+ * @date 2019/6/511:10
+ */
+public class MergeSort {
+
+}
