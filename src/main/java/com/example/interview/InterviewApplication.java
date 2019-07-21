3f2b3c10fcd53d69e14eddb56fd72541264e5a1a
lei1160427600 <xuechao_lei@126.com>
2019-07-21 13:55:05 +0800

    add apollo config

--- src/main/java/com/example/interview/InterviewApplication.java
+++ src/main/java/com/example/interview/InterviewApplication.java
@@ -1,8 +1,12 @@
 package com.example.interview;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.example.interview.dao.UserDao;
 import com.example.interview.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +17,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 //@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @SpringBootApplication(scanBasePackages = "com.example.interview")
 @RestController
+@EnableApolloConfig
 @RequestMapping(value = "/test")
+@Slf4j
 public class InterviewApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(InterviewApplication.class);
     @Autowired
     private UserDao userDao;
+    @ApolloConfig
+    private Config config;
     @Autowired
     private User user;
     @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String sayHello(){
+        Set<String> set = config.getPropertyNames();
         System.out.println("Hello word");
-        System.out.println(JSONObject.toJSONString(user));
-        LOGGER.info("logger...................");
+        System.out.println(JSONObject.toJSONString(set));
+        log.info("logger...................");
         return "Hello word";
     }
+//    static {
+//        System.setProperty("-Dapp.id","123456");
+//        System.setProperty("-Denv","dev");
+//        System.setProperty("-Ddev_meta","http://127.0.0.1:8080");
+//    }
     @RequestMapping(value = "/getUserById/{id}",method = RequestMethod.GET)
     public UserTest getUser(@PathVariable Long id){
         return userDao.getUserById(id);
